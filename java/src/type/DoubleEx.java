47dcee764256a7a8143fd2585657ece90ffa0054
ssong <9yeosong@naver.com>
2025-03-31 16:50:42 +0900

    20250331

---
+++ java/src/type/DoubleEx.java
@@ -0,0 +1,10 @@
+package type;
+
+public class DoubleEx {
+	public static void main(String[] args) {
+		double a = 10.5;
+		System.out.println(a);
+		float b = 10.5f;
+		System.out.println(b);
+	}
+}
