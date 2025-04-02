50fe18660dba6d110eb8f1a811ab11cc32ca9a86
ssong <9yeosong@naver.com>
2025-04-02 17:31:49 +0900

    20250402 html, mysql, java

---
+++ java/src/loop/MultiTable2.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class MultiTable2 {
+	public static void main(String[] args) {
+		for (int i = 2; i <= 9; i++) {
+			System.out.println(i + "단");
+			for (int j = 1; j <= 9; j++) {
+				System.out.println(String.format("%d x %d = %d", i, j, i * j));
+			}
+			System.out.println();
+		}
+	}
+}
