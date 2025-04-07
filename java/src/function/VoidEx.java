3b18a85130151a8fad5a6cd48bb653729377afee
ssong <9yeosong@naver.com>
2025-04-07 17:27:57 +0900

    20250407

---
+++ java/src/function/VoidEx.java
@@ -0,0 +1,13 @@
+package function;
+
+public class VoidEx {
+	static void hello() {
+		System.out.println("hello");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("start");
+		hello();
+		System.out.println("end");
+	}
+}
