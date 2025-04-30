628c7a04697dc7b748dcf147685a648780f765d5
ssong <9yeosong@naver.com>
2025-04-30 18:07:18 +0900

    20250430

--- java_oracle/src/address/AddressList.java
+++ java_oracle/src/address/AddressList.java
@@ -11,14 +11,16 @@ public class AddressList {
 		String url = "jdbc:oracle:thin:@localhost:1521/xe";
 		String id = "java";
 		String pwd = "1234";
-		
+
 		Connection conn = null;
 		Statement pstmt = null;
 		ResultSet rs = null;
 		try {
 			conn = DriverManager.getConnection(url, id, pwd);
 			String sql = "select * from address";
-			//stmt = conn.createStatement(0, 0)
+			// stmt = conn.createStatement(0, 0)
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
 	}
 }
